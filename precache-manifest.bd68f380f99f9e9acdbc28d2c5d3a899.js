0f7b81edb6133c9d7b30d19e367b69ce334c22d3
gilpratama <gilpratama@users.noreply.github.com>
2020-12-23 09:40:59 +0700

    Updates

---
+++ precache-manifest.bd68f380f99f9e9acdbc28d2c5d3a899.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5bc22daf351734a8afb631d581feee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cec974399d3704bb8156",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b8946c932c3bea0c15a7",
+    "url": "/portfolio/static/css/main.5cb2ace2.chunk.css"
+  },
+  {
+    "revision": "cec974399d3704bb8156",
+    "url": "/portfolio/static/js/2.ab756a19.chunk.js"
+  },
+  {
+    "revision": "01f1b5ae975a8d17e413a750f30bfaaa",
+    "url": "/portfolio/static/js/2.ab756a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8946c932c3bea0c15a7",
+    "url": "/portfolio/static/js/main.fefc75c2.chunk.js"
+  },
+  {
+    "revision": "d7d0aed4961f7ff9ccd0",
+    "url": "/portfolio/static/js/runtime-main.39bc2ec3.js"
+  },
+  {
+    "revision": "18e7fad541604561c8e22a36a9185404",
+    "url": "/portfolio/static/media/Coolvetica.ttf.18e7fad5.eot"
+  },
+  {
+    "revision": "5bd516998e00831e7676715db5a71145",
+    "url": "/portfolio/static/media/Coolvetica.ttf.5bd51699.woff"
+  },
+  {
+    "revision": "d562452818cc7b260b814668b1efa3a7",
+    "url": "/portfolio/static/media/Coolvetica.ttf.d5624528.svg"
+  }
+]);
